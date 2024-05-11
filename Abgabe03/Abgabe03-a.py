56fada020637116da6c847b5e7796e6769ef5554
ErKepi <ipek.erkan97@gmail.com>
2024-05-11 19:30:16 +0200

    2024-05-11 19:30

--- Abgabe03/Abgabe03-a.py
+++ Abgabe03/Abgabe03-a.py
@@ -1,5 +1,5 @@
 def f(a, b, c):
-    return (not a and not b and c) or (not a and b and c) or (a and b and not c) or (a and b and c)
+    return (not (a or b) and c) or (not a and b and c) or (a and b and not c) or (a and b and c)
 
 
 print(f(0, 0, 1))
